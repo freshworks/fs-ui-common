63e2fd85cf2533233d6d28465b0cfc7e6d03d226
Deva Manikanta Gupta Potturu <deva.potturu@freshworks.com>
2024-09-19 15:37:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,10 @@ export default () => {
         name: "FS UI Library",
       },
       rollupOptions: {
+        input: {
+          "fs-colors": "./src/styles/colors/main.scss",
+          "fs-button": "./src/styles/components/button/button.scss",
+        },
         output: {
           entryFileNames: `assets/[name].js`,
           chunkFileNames: `assets/[name].js`,
