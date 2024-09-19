ab0c21725b0525985a5a21429954b5cf9ec7d91c
Deva Manikanta Gupta Potturu <deva.potturu@freshworks.com>
2024-09-19 15:28:17 +0530

    updating vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+
+const files = ["colors/main.scss", "components/button/button.scss"];
+
+const entries = files.map((file) => resolve(__dirname, `./src/styles/${file}`));
 
 export default () => {
   return defineConfig({
     build: {
       cssCodeSplit: true,
+      lib: {
+        entry: entries,
+        name: "FS UI Library",
+      },
       rollupOptions: {
         input: {
           "fs-colors": "./src/styles/colors/main.scss",
