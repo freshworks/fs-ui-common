20e2ed820edded946f077550e1ead2d5b6a4fa21
MoghanaPriya <moghanapriya.rangan@freshworks.com>
2025-01-08 14:32:37 +0530

    Created folder structure for build files based on its source folder except for styles

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import fastGlob from 'fast-glob';
+import path from 'path';
 
 const files = ["colors/main.scss", "components/button/button.scss"];
 
-const entries = files.map((file) => resolve(__dirname, `./src/styles/${file}`));
+const entries = files.map((file) => path.resolve(__dirname, `./src/styles/${file}`));
 
 export default () => {
   return defineConfig({
@@ -20,13 +21,43 @@ export default () => {
           "fs-colors-wc": "./src/styles/colors/main-wc.scss",
           "fs-base-styles": "./src/styles/app-components/main.scss",
           "fs-reset": "./src/styles/base/reset.scss",
+          ...generateInputsFromFolder('./src/utils'),
         },
         output: {
-          entryFileNames: `assets/[name].js`,
+          entryFileNames: (chunkInfo) => {
+            const { facadeModuleId } = chunkInfo;
+            // By default entry files are placed under assets folder, to group them under other folders based on module type, add those folders here
+            const moduleTypes = ['utils'];
+            for (let i = 0; i < moduleTypes.length; i++) {
+              if (facadeModuleId?.includes(moduleTypes[i])) {
+                const moduleType = moduleTypes[i];
+                const [, lastPart] = facadeModuleId.split(moduleTypes[i]);
+                const lastFolders = lastPart.slice(
+                  0,
+                  lastPart.lastIndexOf('/'),
+                );
+                return `${moduleType}${lastFolders}/[name].js`;
+              }
+            }
+            return 'assets/[name].js';
+          },
           chunkFileNames: `assets/[name].js`,
           assetFileNames: `assets/styles/[name].[ext]`,
         },
+        external: ['fs', 'child_process']
       },
     },
   });
+
+  function generateInputsFromFolder(folderPath) {
+    const files = fastGlob.sync(`${folderPath}/**/*.*`); // Matches all files in the folder
+    const inputs = {};
+
+    files.forEach((file) => {
+      const fileName = path.basename(file, path.extname(file)); // e.g., 'svg-optimiser'
+      inputs[fileName] = file;
+    });
+
+    return inputs;
+  }
 };
